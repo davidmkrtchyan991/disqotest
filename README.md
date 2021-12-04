72aa65fcab346263fa848b1a8a26eca72fb0dd52
David <d.mkrtchyan@whitewill.ru>
2021-12-04 22:43:13 +0400

    Added reset and variables

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hello-world
+# DISQO Test
 
 ## Project setup
 ```
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <p>Some text</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
