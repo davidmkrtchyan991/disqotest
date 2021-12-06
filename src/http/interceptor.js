ca6b83661cfbd180fead14a88a63837443f15bda
David <d.mkrtchyan@whitewill.ru>
2021-12-07 02:03:20 +0400

    Added crypto to hide token from git visibility

--- src/http/interceptor.js
+++ src/http/interceptor.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import CryptoJS from 'crypto-js';
 
 export default function setup() {
-    const token = "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";
+    const cipher = 'Z2hwX2pKQ2tRNDV3UE1BWnJ4RTFYQzhQQk1KcmVqOVh5bDNwTTBnZQ==';
+    const token = CryptoJS.enc.Base64.parse(cipher).toString(CryptoJS.enc.Utf8);
     axios.interceptors.request.use(function (config) {
         config.headers.Authorization = `token ${token}`;
         return config;
